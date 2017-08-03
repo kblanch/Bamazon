dfe41dd185f75b31ddcc17e1fbe6880a02b199d6
Karen <K.Blanchette07@gmail.com>
2017-08-03 13:46:39 -0500

    Switch Options

--- bamazonManager.js
+++ bamazonManager.js
@@ -5,7 +5,7 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 
 //First
-//List a Set of Menu Options
+//List a Set of Menu Options (Use Inquirer)
 inquirer.prompt([
     {
         type: 'list',
@@ -14,18 +14,47 @@ inquirer.prompt([
         choices:['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
     }
 ]).then(function(userSelection){
-    console.log(userSelection.option)
+    switch(userSelection.option){
+        case "View Products for Sale":
+            viewProducts();
+            break;
+        case "View Low Inventory":
+            viewLowInventory();
+            break;
+        case "Add to Inventory":
+            addInventory();
+            break;
+        case "Add New Product":
+            addProduct();
+            break;
+        default:
+            console.log('Error');
+            break;
+    }
 });
 
 
 //View Products for Sales
 //List every available item
 //Item, ID, Name, Price, Qty
+function viewProducts(){
+    console.log('View Products');
+}
+
 
 //View Low Inventory
 //List items with inventory count lower than 5
+function viewLowInventory(){
+    console.log("View Low Inv");
+}
 
 //Add to Inventory
 //Display prompt let manager manager add to inv
+function addInventory(){
+    console.log("Add Inventory");
+}
 
 //Add New Product
+function addProduct(){
+    console.log("Add Product");
+}
